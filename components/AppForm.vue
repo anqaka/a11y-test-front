f0d1c89e2b05ef349480ea9339a40e591f0b22a5
Zuza <zuzanna.szymanda@snow.dog>
2021-06-25 11:55:18 +0200

    init form

---
+++ components/AppForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <form @submit.prevent>
+    <CFormControl py="2">
+      <c-form-label for="pages">Pages</c-form-label>
+      <c-textarea
+        required
+        placeholder="Pages"
+        id="pages"
+        v-model="form.pages"
+      />
+      <c-form-helper-text>Divide pages with comma</c-form-helper-text>
+    </CFormControl>
+    <CFormControl py="2">
+      <c-form-label for="title">Title</c-form-label>
+      <c-input placeholder="Title" id="title" v-model="form.title" />
+    </CFormControl>
+    <CFormControl py="2">
+      <c-form-label for="fileName">Filename</c-form-label>
+      <c-input placeholder="Filename" id="fileName" v-model="form.fileName" />
+    </CFormControl>
+    <CFormControl py="2">
+      <c-form-label for="resultsDir">Results dir</c-form-label>
+      <c-input placeholder="Title" id="resultsDir" v-model="form.resultsDir" />
+    </CFormControl>
+    <CHeading as="h2" size="sm" pt="4">
+      Axe config
+    </CHeading>
+    <CDivider />
+    <CFormControl py="2">
+      <c-form-label for="reporter">Reporter</c-form-label>
+      <c-input
+        placeholder="Reporter"
+        id="reporter"
+        v-model="form.axeConfig.reporter"
+      />
+    </CFormControl>
+    <CHeading as="h2" size="sm" pt="4">
+      Viewport
+    </CHeading>
+    <CDivider />
+    <CFormControl py="2">
+      <c-form-label for="width">Width (px)</c-form-label>
+      <c-input
+        type="number"
+        placeholder="Width"
+        id="width"
+        v-model="form.viewport.width"
+      />
+    </CFormControl>
+    <CFormControl py="2">
+      <c-form-label for="height">Height (px)</c-form-label>
+      <c-input
+        type="number"
+        placeholder="Height"
+        id="height"
+        v-model="form.viewport.height"
+      />
+    </CFormControl>
+    <CHeading as="h2" size="sm" pt="4">
+      Basic auth configuration
+    </CHeading>
+    <CDivider />
+    <CFormControl py="2">
+      <c-form-label for="username">Username</c-form-label>
+      <c-input
+        placeholder="Username"
+        id="username"
+        v-model="form.basicAuth.username"
+      />
+    </CFormControl>
+    <CFormControl py="2">
+      <c-form-label for="password">Password</c-form-label>
+      <c-input
+        type="password"
+        placeholder="Password"
+        id="password"
+        v-model="form.basicAuth.password"
+      />
+    </CFormControl>
+    <CButton @click="onSubmit">
+      Send
+    </CButton>
+  </form>
+</template>
+
+<script>
+import {
+  CFormControl,
+  CFormLabel,
+  CInput,
+  CTextarea,
+  CFormHelperText,
+  CDivider,
+  CHeading,
+  CButton
+} from '@chakra-ui/vue'
+
+export default {
+  components: {
+    CFormControl,
+    CFormLabel,
+    CFormHelperText,
+    CTextarea,
+    CInput,
+    CDivider,
+    CHeading,
+    CButton
+  },
+
+  data() {
+    return {
+      form: {
+        pages: [],
+        title: '',
+        fileName: '',
+        resultsDir: '',
+        basicAuth: {
+          username: '',
+          password: ''
+        },
+        axeConfig: {
+          reporter: ''
+        },
+        viewport: {
+          width: null,
+          height: null
+        }
+      }
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      this.$emit('submit', this.form);
+    }
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,14 @@
       <CHeading text-align="center" mb="4">
         A11y testing with axe
       </CHeading>
+      <c-box
+        :w="['100vw', '30vw']"
+        boxShadow=" 0 4px 6px 0 rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)"
+        p="10"
+        mr="10"
+      >
+        <app-form @submit="onSubmit" />
+      </c-box>
     </c-box>
   </div>
 </template>
@@ -55,14 +63,18 @@ export default {
     }
   },
   methods: {
-    showToast () {
+    showToast (title, message, status = 'success') {
       this.$toast({
-        title: 'Account created.',
-        description: "We've created your account for you.",
-        status: 'success',
+        title,
+        description: message,
+        status,
         duration: 10000,
         isClosable: true
       })
+    },
+
+    async onSubmit (data) {
+      // todo
     }
   }
 }
